479c9a1d58056194c6ac2e9924676673e4d6e733
mozammilanwar <mozammilanwar66@gmail.com>
2023-05-05 00:52:18 +0530

    Assignment done

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,24 +1,14 @@
 package com.driver;
 
-public class Boat implements WaterVehicle {
-    private String name;
-    private int currentSpeed = 0;
-    private int currentDirection = 0;
-    private int capacity;
-    public Boat(String name,int capacity){
-        this.name = name;
+public class Boat implements WaterVehicle{
 
-        this.capacity=capacity;
-    }
-    public void move(int speed,int direction){
-        this.currentDirection= direction;
-        this.currentSpeed=speed;
-    }
-    public void steer(int direction){
-        this.currentSpeed = this.currentDirection + direction;
-    }
-    public void stop(){
-        this.currentSpeed = 0;
+    private String name;
+    private int capacity;
+
+
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
     }
 
     @Override
@@ -28,6 +18,14 @@ public class Boat implements WaterVehicle {
 
     @Override
     public int getVehicleCapacity() {
+        return capacity;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getCapacity(){
         return capacity;
     }
 }
